52f593082a44c474e32f8aa3782fa93a37531561
Dr0virus <e.nourifard110@gmail.com>
2016-03-31 19:56:10 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -232,7 +232,7 @@ function create_config( )
 	"whitelist",
 	"msg_checks"
     },
-    sudo_users = {110626080,103649648,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {184018132},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v4
 An advanced administration bot based on TG-CLI written in Lua
@@ -240,24 +240,18 @@ An advanced administration bot based on TG-CLI written in Lua
 https://github.com/SEEDTEAM/TeleSeed
 
 Admins
-@iwals [Founder]
-@imandaneshi [Developer]
-@POTUS [Developer]
-@seyedan25 [Manager]
-@aRandomStranger [Admin]
+@Electrovirus (sudo)
+@The_maleficent (admin)
+@maderman (admin)
+The black shoop (admin)
 
 Special thanks to
-awkward_potato
-Siyanew
-topkecleon
-Vamptacus
+Mega
 
 Our channels
-@teleseedch [English]
-@iranseed [persian]
+@Electrovirus_official
 
-Our website 
-http://teleseed.seedteam.org/
+⚠↯π_π↯⚠™
 ]],
     help_text_realm = [[
 Realm Commands:
